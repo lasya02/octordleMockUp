e2f1f2258fb5149768c91cf4e579b8ee6d670182
Lasya Kambhampati <84462162+lasya02@users.noreply.github.com>
2023-07-25 11:15:05 -0400

    begin the mockup

---
+++ OctordleMockUp/OctordleMockUp/OctordleMockUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  OctordleMockUpApp.swift
+//  OctordleMockUp
+//
+//  Created by Lasya Kambhampati on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct OctordleMockUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
